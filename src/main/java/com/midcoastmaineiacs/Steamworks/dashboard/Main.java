4cba67e17bd6bab52b9c3de1329d7c1346f188f4
Tupster24 <tupperkion@gmail.com>
2017-10-08 17:39:32 -0400

    Wrote first version of new dashboard

--- src/main/java/com/midcoastmaineiacs/Steamworks/dashboard/Main.java
+++ src/main/java/com/midcoastmaineiacs/Steamworks/dashboard/Main.java
@@ -1,41 +1,52 @@
 package com.midcoastmaineiacs.Steamworks.dashboard;
 
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.events.TitleEvent;
+import com.teamdev.jxbrowser.chromium.events.TitleListener;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Region;
-import javafx.scene.web.WebEngine;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main extends Application {
+	public static Stage stage;
+
 	@Override
 	public void start(Stage stage) throws Exception {
+		Main.stage = stage;
 		stage.setMaximized(true);
 		stage.setTitle("Jeffrey");
 		WebView webview = new WebView();
-		Scene scene = new Scene(new Browser(), stage.getWidth(), stage.getHeight());
+		StackPane pane = new StackPane();
+		Browser browser = new Browser();
+		BrowserView view = new BrowserView(browser);
+		pane.getChildren().add(view);
+		Scene scene = new Scene(pane, stage.getWidth(), stage.getHeight());
 		stage.setScene(scene);
 		stage.show();
-	}
-}
-
-class Browser extends Region {
-	private final WebView browser = new WebView();
-	private final WebEngine engine = browser.getEngine();
-
-	public Browser() {
-		getStyleClass().add("browser");
-		engine.load("file:///media/tupster24/Robotics/Steamworks Dashboard/index.html");
-		getChildren().add(browser);
+		String index = readFile("index.html").toString();
+		String jq = readFile("jquery-3.2.1.slim.min.js").toString();
+		browser.loadHTML(index.replace(" src=\"jquery-3.2.1.slim.min.js\">", ">" + jq));
 	}
 
-	@Override
-	protected void layoutChildren() {
-		double w = getWidth();
-		double h = getHeight();
-		layoutInArea(browser,0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+	private static StringBuilder readFile(String name) {
+		BufferedReader index = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(name)));
+		StringBuilder page = new StringBuilder();
+		try {
+			String line = index.readLine();
+			while (line != null) {
+				page.append(line);
+				line = index.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return page;
 	}
 }
